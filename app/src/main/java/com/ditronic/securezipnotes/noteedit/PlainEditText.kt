635eb4259ebfaaf94cb2220330f519b492562158
Felix Kirchengast <felix.kirchengast@gmail.com>
2019-11-04 22:41:39 +0100

    Fix EditText lint

--- app/src/main/java/com/ditronic/securezipnotes/noteedit/PlainEditText.kt
+++ app/src/main/java/com/ditronic/securezipnotes/noteedit/PlainEditText.kt
@@ -1,20 +1,16 @@
 package com.ditronic.securezipnotes.noteedit
 
-import android.annotation.TargetApi
 import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
-import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
 
-class PlainEditText : EditText {
-    constructor(context: Context) : super(context) {}
+class PlainEditText : AppCompatEditText {
+    constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-
-    @TargetApi(21)
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
     override fun onTextContextMenuItem(id: Int): Boolean {
         var newId = id
