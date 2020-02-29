5d1091e1abde5e3a0543bb1b7590b909930f9663
Felix Kirchengast <mail@felixkirchengast.at>
2020-02-29 15:15:40 +0100

    Fix test script

--- run_instrumentation_tests.sh
+++ run_instrumentation_tests.sh
@@ -2,12 +2,16 @@
 set -e
 set -x
 
-# TODO: Remove hardcoded paths
+# This is just an experimental script to capture some commands.
+# Real testing is performed via gradle tasks or Android Studio.
 
 # Debug
 TARGET_APP=com.ditronic.securezipnotes.dev
 # Release
 #TARGET_APP=com.ditronic.securezipnotes
+
+WHICH_TESTS='com.ditronic.securezipnotes.tests.ITest#deleteLastNote'
+ORCHESTRATOR_VERSION='1.2.0'
 
 # Install app
 adb push app/build/outputs/apk/debug/app-debug.apk /data/local/tmp/$TARGET_APP
@@ -18,14 +22,12 @@ adb push app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk /data
 adb shell pm install --full -t -r "/data/local/tmp/$TARGET_APP.test"
 
 # Install orchestrator app
-adb push /home/felix/.gradle/caches/modules-2/files-2.1/androidx.test/orchestrator/1.2.0/e3edf4e08d2d3db127c1b56bbe405b90b64daa5/orchestrator-1.2.0.apk /data/local/tmp/androidx.test.orchestrator
+adb push ~/.gradle/caches/modules-2/files-2.1/androidx.test/orchestrator/$ORCHESTRATOR_VERSION/e3edf4e08d2d3db127c1b56bbe405b90b64daa5/orchestrator-$ORCHESTRATOR_VERSION.apk /data/local/tmp/androidx.test.orchestrator
 adb shell pm install --full -t -r "/data/local/tmp/androidx.test.orchestrator"
 
 # Install test services apk
-adb push /home/felix/.gradle/caches/modules-2/files-2.1/androidx.test.services/test-services/1.2.0/58c9ac48a725a1a0437f4f6be352a42f60ed5a7d/test-services-1.2.0.apk /data/local/tmp/androidx.test.services
+adb push ~/.gradle/caches/modules-2/files-2.1/androidx.test.services/test-services/$ORCHESTRATOR_VERSION/58c9ac48a725a1a0437f4f6be352a42f60ed5a7d/test-services-$ORCHESTRATOR_VERSION.apk /data/local/tmp/androidx.test.services
 adb shell pm install --full -t -r "/data/local/tmp/androidx.test.services"
 
-
 # Run the tests
-adb shell 'CLASSPATH=$(pm path androidx.test.services)' app_process / androidx.test.services.shellexecutor.ShellMain am instrument -r -w -e targetInstrumentation $TARGET_APP.test/androidx.test.runner.AndroidJUnitRunner   -e debug false -e class 'com.ditronic.securezipnotes.ITest' -e clearPackageData true androidx.test.orchestrator/androidx.test.orchestrator.AndroidTestOrchestrator
-
+adb shell 'CLASSPATH=$(pm path androidx.test.services)' app_process / androidx.test.services.shellexecutor.ShellMain am instrument -r -w -e targetInstrumentation $TARGET_APP.test/androidx.test.runner.AndroidJUnitRunner -e debug false -e class $WHICH_TESTS -e clearPackageData true androidx.test.orchestrator/androidx.test.orchestrator.AndroidTestOrchestrator
